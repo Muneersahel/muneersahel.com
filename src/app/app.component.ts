c90b67c65d17501273291c30127fd216cc23577b
Muneersahel <saidmunir73@gmail.com>
2023-11-16 09:15:16 +0300

    add tailwind and prettier

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,14 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	standalone: true,
+	imports: [CommonModule, RouterOutlet],
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'muneersahel.com';
+	title = 'muneersahel.com';
 }
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{html,ts}'],
+	theme: {
+		container: {
+			center: true,
+			padding: '1rem',
+		},
+		extend: {},
+	},
+	plugins: [],
+};
