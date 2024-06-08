3276b7203a5e8a0243d2c1a3e0825755440aa42a
Muneersahel <saidmunir73@gmail.com>
2024-06-08 10:29:26 +0300

    chore: Add Firebase configuration files
    The code changes include adding the necessary Firebase configuration files (.firebaserc, firestore.indexes.json, firebase.json, firestore.rules) and updating the .gitignore file to exclude Firebase related files. This commit sets up the Firebase project configuration for the repository.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,12 @@
 import { HttpClient, provideHttpClient, withFetch } from '@angular/common/http';
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {
+  ScreenTrackingService,
+  getAnalytics,
+  provideAnalytics,
+} from '@angular/fire/analytics';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import {
@@ -18,5 +25,20 @@ export const appConfig: ApplicationConfig = {
     provideAnimationsAsync(),
     provideHttpClient(withFetch()),
     provideMarkdown({ loader: HttpClient }),
+    provideFirebaseApp(() =>
+      initializeApp({
+        projectId: 'muneersahel',
+        appId: '1:1049500314015:web:b0f618cd743a58a9b3b5aa',
+        storageBucket: 'muneersahel.appspot.com',
+        // locationId: 'us-central',
+        apiKey: 'AIzaSyAsjcjQCJvJuJcZPt_5r0M5OLX5ZGP8te8',
+        authDomain: 'muneersahel.firebaseapp.com',
+        messagingSenderId: '1049500314015',
+        measurementId: 'G-G2BMEHD10R',
+      }),
+    ),
+    provideAnalytics(() => getAnalytics()),
+    ScreenTrackingService,
+    provideFirestore(() => getFirestore()),
   ],
 };
